8d58eb20963d751d086e309b5c6ae6618c0e9d86
Amine Rhouma <amine.rhouma@deliveryhero.com>
2023-02-03 00:10:40 +0100

    updated markwhen content

---
+++ src/gowebTimeline.ts
@@ -0,0 +1,213 @@
+
+export const gowebTimeline = `
+title: GoWeb release timeline 2023
+dateFormat: d/M/y
+
+// Tags
+#regular: green
+#fasttrack: blue
+#hotfix: red
+
+// Firefighters
+#amine: purple
+#anto: gold
+#ashutosh: orange
+#bassem: cadetblue
+#jasim: brown
+#laszlo: greenyellow
+#poulami: lightseagreen
+#viktoriya: pink
+
+
+section Regular #regular
+group dc96c8647c55ef8fed43ed903db433c6cba1f87e
+09/01/2023 8:00am: ME_SA    #ashutosh
+change-logs:
+- Revert "VLE-333: use new audio library for sound notifications (#5201)" (#5352)
+- VCF-523/part: Using copy of en.json for TestTranslateModule (#5344)
+- VCF-526: migrate testing utils to shared lib (#5332)
+- VLI-422: Login and language scenarios in Playwright (#5366)
+- VLI-436: Performance tests - remove Protractor version (#5373)
+- VOR-465: responsive height for new order and decline modal (#5368)
+- DX: apply CS (#5346)
+- DX: Copy translation file before yarn start (#5380)
+- DX: Fix unit tests (#5387)
+- DX: opening password prompt modal - mode detailed log (#5343)
+- DX: password prompt not suggested - demote log to debug (#5354)
+- DX: Refactoring the earn points method for Rewards MVP (#5357)
+- DX: Removed contract tests from Unit Tests (#5382)
+- DX: rename GlobalErrorHandler file (#5367)
+
+10/01/2023 8:00am: ME_AE    #bassem
+change-logs:
+- Revert "VLE-333: use new audio library for sound notifications (#5201)" (#5352)
+- VCF-523/part: Using copy of en.json for TestTranslateModule (#5344)
+- VCF-526: migrate testing utils to shared lib (#5332)
+- VLI-422: Login and language scenarios in Playwright (#5366)
+- VLI-436: Performance tests - remove Protractor version (#5373)
+- VOR-465: responsive height for new order and decline modal (#5368)
+- DX: apply CS (#5346)
+- DX: Copy translation file before yarn start (#5380)
+- DX: Fix unit tests (#5387)
+- DX: opening password prompt modal - mode detailed log (#5343)
+- DX: password prompt not suggested - demote log to debug (#5354)
+- DX: Refactoring the earn points method for Rewards MVP (#5357)
+- DX: Removed contract tests from Unit Tests (#5382)
+- DX: rename GlobalErrorHandler file (#5367)
+
+11/01/2023 8:00am: ME_KW    #viktoriya
+change-logs:
+- Revert "VLE-333: use new audio library for sound notifications (#5201)" (#5352)
+- VCF-523/part: Using copy of en.json for TestTranslateModule (#5344)
+- VCF-526: migrate testing utils to shared lib (#5332)
+- VLI-422: Login and language scenarios in Playwright (#5366)
+- VLI-436: Performance tests - remove Protractor version (#5373)
+- VOR-465: responsive height for new order and decline modal (#5368)
+- DX: apply CS (#5346)
+- DX: Copy translation file before yarn start (#5380)
+- DX: Fix unit tests (#5387)
+- DX: opening password prompt modal - mode detailed log (#5343)
+- DX: password prompt not suggested - demote log to debug (#5354)
+- DX: Refactoring the earn points method for Rewards MVP (#5357)
+- DX: Removed contract tests from Unit Tests (#5382)
+- DX: rename GlobalErrorHandler file (#5367)
+
+16/01/2023 8:00am: ME_QA    #poulami
+change-logs:
+- Revert "VLE-333: use new audio library for sound notifications (#5201)" (#5352)
+- VCF-523/part: Using copy of en.json for TestTranslateModule (#5344)
+- VCF-526: migrate testing utils to shared lib (#5332)
+- VLI-422: Login and language scenarios in Playwright (#5366)
+- VLI-436: Performance tests - remove Protractor version (#5373)
+- VOR-465: responsive height for new order and decline modal (#5368)
+- DX: apply CS (#5346)
+- DX: Copy translation file before yarn start (#5380)
+- DX: Fix unit tests (#5387)
+- DX: opening password prompt modal - mode detailed log (#5343)
+- DX: password prompt not suggested - demote log to debug (#5354)
+- DX: Refactoring the earn points method for Rewards MVP (#5357)
+- DX: Removed contract tests from Unit Tests (#5382)
+- DX: rename GlobalErrorHandler file (#5367)
+
+17/01/2023 8:00am: ME       #amine
+change-logs:
+- Revert "VLE-333: use new audio library for sound notifications (#5201)" (#5352)
+- VCF-523/part: Using copy of en.json for TestTranslateModule (#5344)
+- VCF-526: migrate testing utils to shared lib (#5332)
+- VLI-422: Login and language scenarios in Playwright (#5366)
+- VLI-436: Performance tests - remove Protractor version (#5373)
+- VOR-465: responsive height for new order and decline modal (#5368)
+- DX: apply CS (#5346)
+- DX: Copy translation file before yarn start (#5380)
+- DX: Fix unit tests (#5387)
+- DX: opening password prompt modal - mode detailed log (#5343)
+- DX: password prompt not suggested - demote log to debug (#5354)
+- DX: Refactoring the earn points method for Rewards MVP (#5357)
+- DX: Removed contract tests from Unit Tests (#5382)
+- DX: rename GlobalErrorHandler file (#5367)
+
+18/01/2023 8:00am: US_CL    #anto
+change-logs:
+- Revert "VLE-333: use new audio library for sound notifications (#5201)" (#5352)
+- VCF-523/part: Using copy of en.json for TestTranslateModule (#5344)
+- VCF-526: migrate testing utils to shared lib (#5332)
+- VLI-422: Login and language scenarios in Playwright (#5366)
+- VLI-436: Performance tests - remove Protractor version (#5373)
+- VOR-465: responsive height for new order and decline modal (#5368)
+- DX: apply CS (#5346)
+- DX: Copy translation file before yarn start (#5380)
+- DX: Fix unit tests (#5387)
+- DX: opening password prompt modal - mode detailed log (#5343)
+- DX: password prompt not suggested - demote log to debug (#5354)
+- DX: Refactoring the earn points method for Rewards MVP (#5357)
+- DX: Removed contract tests from Unit Tests (#5382)
+- DX: rename GlobalErrorHandler file (#5367)
+
+19/01/2023 8:00am: US_AR    #bassem
+change-logs:
+- Revert "VLE-333: use new audio library for sound notifications (#5201)" (#5352)
+- VCF-523/part: Using copy of en.json for TestTranslateModule (#5344)
+- VCF-526: migrate testing utils to shared lib (#5332)
+- VLI-422: Login and language scenarios in Playwright (#5366)
+- VLI-436: Performance tests - remove Protractor version (#5373)
+- VOR-465: responsive height for new order and decline modal (#5368)
+- DX: apply CS (#5346)
+- DX: Copy translation file before yarn start (#5380)
+- DX: Fix unit tests (#5387)
+- DX: opening password prompt modal - mode detailed log (#5343)
+- DX: password prompt not suggested - demote log to debug (#5354)
+- DX: Refactoring the earn points method for Rewards MVP (#5357)
+- DX: Removed contract tests from Unit Tests (#5382)
+- DX: rename GlobalErrorHandler file (#5367)
+
+23/01/2023 8:00am: EU+US    #ashutosh
+change-logs:
+- Revert "VLE-333: use new audio library for sound notifications (#5201)" (#5352)
+- VCF-523/part: Using copy of en.json for TestTranslateModule (#5344)
+- VCF-526: migrate testing utils to shared lib (#5332)
+- VLI-422: Login and language scenarios in Playwright (#5366)
+- VLI-436: Performance tests - remove Protractor version (#5373)
+- VOR-465: responsive height for new order and decline modal (#5368)
+- DX: apply CS (#5346)
+- DX: Copy translation file before yarn start (#5380)
+- DX: Fix unit tests (#5387)
+- DX: opening password prompt modal - mode detailed log (#5343)
+- DX: password prompt not suggested - demote log to debug (#5354)
+- DX: Refactoring the earn points method for Rewards MVP (#5357)
+- DX: Removed contract tests from Unit Tests (#5382)
+- DX: rename GlobalErrorHandler file (#5367)
+
+24/01/2023 8:00am: ME_TR+AS #laszlo
+change-logs:
+- Revert "VLE-333: use new audio library for sound notifications (#5201)" (#5352)
+- VCF-523/part: Using copy of en.json for TestTranslateModule (#5344)
+- VCF-526: migrate testing utils to shared lib (#5332)
+- VLI-422: Login and language scenarios in Playwright (#5366)
+- VLI-436: Performance tests - remove Protractor version (#5373)
+- VOR-465: responsive height for new order and decline modal (#5368)
+- DX: apply CS (#5346)
+- DX: Copy translation file before yarn start (#5380)
+- DX: Fix unit tests (#5387)
+- DX: opening password prompt modal - mode detailed log (#5343)
+- DX: password prompt not suggested - demote log to debug (#5354)
+- DX: Refactoring the earn points method for Rewards MVP (#5357)
+- DX: Removed contract tests from Unit Tests (#5382)
+- DX: rename GlobalErrorHandler file (#5367)
+
+endGroup
+endSection
+
+section Fast-track #fasttrack
+group AS
+27/12/2022 8:00am: 41e1cf23496080d8a7c3d7adbbfebf210ba41ce1    #ashutosh
+
+24/01/2023 8:00am: 81c49fcda0d449133fce276755e51badf6e35ae6    #anto
+checkedout from dc96c8647c55ef8fed43ed903db433c6cba1f87e and then merged into fasttrack/apac and then released
+
+25/01/2023 4:20pm: efb25356f93889b743fe822bea730f14ff3ef1b3    #laszlo
+checkedout from dc96c8647c55ef8fed43ed903db433c6cba1f87e and then merged into fasttrack/apac and then released
+
+01/02/2023 5:00pm: 26b635651c4808d99fbf038be7343451aa3cf132    #bassem
+cherry-picked Disable UUID option in APAC & some log changes from PR
+
+01/02/2023 5:17pm: 2f0cbc1bfb89ba8173b22ebfc2b2e705a59bf12d    #ashutosh
+cherry picked VOD-411 push-notification-ack
+endGroup
+
+group EU
+24/01/2023 8:00am: ef39a8a9fb4f7668913a3f528b1b7ba6fa230c0f    #amine
+checkedout from dc96c8647c55ef8fed43ed903db433c6cba1f87e and then merged into fasttrack/VLE-398 and then released
+endGroup
+endSection
+
+section Hotfix #hotfix
+25/01/2023 8:19am: ME_KW #jasim
+commit: d04233b7ecbfff651a4980dd7e6fa9ba602f636b
+
+25/01/2023 9:32am: ME_SA #jasim
+commit: d04233b7ecbfff651a4980dd7e6fa9ba602f636b
+
+25/01/2023 9:36am: ME_AE #jasim
+commit: d04233b7ecbfff651a4980dd7e6fa9ba602f636b
+endSection
+`;
